fe13f7b82acb79d62fc9c59cfda1410338ed726d
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-11-29 16:24:43 +0600

    Tabs complet

--- src/pages/Home/components/TourTypeSection.jsx
+++ src/pages/Home/components/TourTypeSection.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const TourTypeSection = () => {
-
     const selectArea = [
         'City Tours',
         'Cultural Tours',
@@ -15,17 +14,13 @@ const TourTypeSection = () => {
         'Educational Tours',
         'Sports Tours'
     ]
-
-
     return (
         <div className='dark:bg-gray-900'>
             <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
                 <div class="max-w-2xl mx-auto text-center py-10">
                     <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Our Most Famous Tour Type  </h2>
                 </div>
-
                 <div class="flex items-center justify-center  flex-wrap lg:px-36 px-6">
-
                     {
                         selectArea?.map((item, i) => {
                             return <Link to={`/tourtype/${item}`} key={i} class="col-span-6 text-center border px-8 py-6 cursor-pointer card-ture">
@@ -38,12 +33,8 @@ const TourTypeSection = () => {
                             </Link>
                         })
                     }
-
                 </div>
-
             </div>
-
-
         </div>
     );
 };
