b6074fc01d17ffdb2c08829e37aa4f628087ff7f
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-11-29 16:17:30 +0600

    All route setup

--- src/layouts/User/UserRoot.jsx
+++ src/layouts/User/UserRoot.jsx
@@ -14,14 +14,12 @@ const UserRoot = () => {
                 <div className="bg-gray-50 dark:bg-slate-900 pt-[58px] md:pt-[90px] lg:pt-0">
                     <div className="sticky inset-x-0 top-0 z-20 px-4 bg-white border-y sm:px-6 md:px-8 lg:hidden dark:bg-gray-800 dark:border-gray-700">
                         <div className="flex items-center py-4">
-
                             <button type="button" className="text-gray-500 hover:text-gray-600" data-hs-overlay="#application-sidebar" aria-controls="application-sidebar" aria-label="Toggle navigation">
                                 <span className="sr-only">Toggle Navigation</span>
                                 <svg className="w-5 h-5" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                     <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                                 </svg>
                             </button>
-
                             <ol className="flex items-center ms-3 whitespace-nowrap" aria-label="Breadcrumb">
                                 <li className="flex items-center text-sm text-gray-800 dark:text-gray-400">
                                     Application Layout
@@ -35,15 +33,12 @@ const UserRoot = () => {
                             </ol>
                         </div>
                     </div>
-
                     <div id="application-sidebar" className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 dark:border-gray-700">
                         <div className="px-6">
                             <Link to='/' className="flex-none text-xl font-semibold dark:text-white" aria-label="Brand">Trips Travel</Link>
                         </div>
-
                         <nav className="flex flex-col flex-wrap w-full p-6 hs-accordion-group" data-hs-accordion-always-open>
                             <ul className="space-y-1.5">
-
                                 <li>
                                     <NavLink
                                         className={({ isActive, isPending }) =>
@@ -79,7 +74,6 @@ const UserRoot = () => {
                             </ul>
                         </nav>
                     </div>
-
                     <div className="w-full px-4 pt-10 sm:px-6 md:px-8 lg:ps-72">
                         <header className='lg:pt-[45px] pt-0'>
                             <Outlet></Outlet>
