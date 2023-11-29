41c27d0a4059d9113471eab1c07d7e840eef79eb
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-11-29 16:30:38 +0600

    All Other pages

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -8,35 +8,27 @@ const Blogs = () => {
                 <title>TripsTravel | Blog</title>
                 <meta name="description" content="Helmet application" />
             </Helmet>
-
             <div class="max-w-3xl px-4 pt-6 lg:pt-10 pb-12 sm:px-6 lg:px-8 mx-auto">
                 <div class="max-w-2xl">
-
                     <div class="flex justify-between items-center mb-6">
                         <div class="flex w-full sm:items-center gap-x-5 sm:gap-x-3">
                             <div class="flex-shrink-0">
                                 <img class="h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1669837401587-f9a4cfe3126e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Image Description" />
                             </div>
-
                             <div class="grow">
                                 <div class="flex justify-between items-center gap-x-2">
                                     <div>
-
                                         <div class="hs-tooltip inline-block [--trigger:hover] [--placement:bottom]">
                                             <div class="hs-tooltip-toggle sm:mb-1 block text-start cursor-pointer">
                                                 <span class="font-semibold text-gray-800 dark:text-gray-200">
                                                     Leyla Ludic
                                                 </span>
-
-
                                                 <div class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 max-w-xs cursor-default bg-gray-900 divide-y divide-gray-700 shadow-lg rounded-xl dark:bg-black" role="tooltip">
-
                                                     <div class="p-4 sm:p-5">
                                                         <div class="mb-2 flex w-full sm:items-center gap-x-5 sm:gap-x-3">
                                                             <div class="flex-shrink-0">
                                                                 <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1669837401587-f9a4cfe3126e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Image Description" />
                                                             </div>
-
                                                             <div class="grow">
                                                                 <p class="text-lg font-semibold text-gray-200">
                                                                     Leyla Ludic
@@ -47,7 +39,6 @@ const Blogs = () => {
                                                             Leyla is a Customer Success Specialist at Preline and spends her time speaking to in-house recruiters all over the world.
                                                         </p>
                                                     </div>
-
                                                     <div class="flex justify-between items-center px-4 py-3 sm:px-5">
                                                         <ul class="text-xs space-x-3">
                                                             <li class="inline-block">
@@ -59,7 +50,6 @@ const Blogs = () => {
                                                                 <span class="text-gray-400">followers</span>
                                                             </li>
                                                         </ul>
-
                                                         <div>
                                                             <button type="button" class="py-1.5 px-2.5 inline-flex items-center gap-x-2 text-xs font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
                                                                 <svg class="flex-shrink-0 w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
@@ -70,13 +60,9 @@ const Blogs = () => {
                                                             </button>
                                                         </div>
                                                     </div>
-
                                                 </div>
-
                                             </div>
                                         </div>
-
-
                                         <ul class="text-xs text-gray-500">
                                             <li class="inline-block relative pe-6 last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-2 before:-translate-y-1/2 before:w-1 before:h-1 before:bg-gray-300 before:rounded-full dark:text-gray-400 dark:before:bg-gray-600">
                                                 Jan 18
@@ -86,7 +72,6 @@ const Blogs = () => {
                                             </li>
                                         </ul>
                                     </div>
-
                                     <div>
                                         <button type="button" class="py-1.5 px-2.5 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
                                             <svg class="w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
@@ -95,32 +80,25 @@ const Blogs = () => {
                                             Tweet
                                         </button>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
                     </div>
-
                     <div class="space-y-5 md:space-y-8">
                         <div class="space-y-3">
                             <h2 class="text-2xl font-bold md:text-3xl dark:text-white">Announcing a free plan for small teams</h2>
 
                             <p class="text-lg text-gray-800 dark:text-gray-200">At preline, our mission has always been focused on bringing openness and transparency to the design process. We've always believed that by providing a space where designers can share ongoing work not only empowers them to make better products, it also helps them grow.</p>
                         </div>
-
                         <p class="text-lg text-gray-800 dark:text-gray-200">We're proud to be a part of creating a more open culture and to continue building a product that supports this vision.</p>
-
                         <figure>
                             <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1670272505340-d906d8d77d03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="Image Description" />
                             <figcaption class="mt-3 text-sm text-center text-gray-500">
                                 A woman sitting at a table.
                             </figcaption>
                         </figure>
-
                         <p class="text-lg text-gray-800 dark:text-gray-200">As we've grown, we've seen how Preline has helped companies such as Spotify, Microsoft, Airbnb, Facebook, and Intercom bring their designers closer together to create amazing things. We've also learned that when the culture of sharing is brought in earlier, the better teams adapt and communicate with one another.</p>
-
                         <p class="text-lg text-gray-800 dark:text-gray-200">That's why we are excited to share that we now have a <a class="text-blue-600 decoration-2 hover:underline font-medium" href="#">free version of Preline</a>, which will allow individual designers, startups and other small teams a chance to create a culture of openness early on.</p>
-
                         <blockquote class="text-center p-4 sm:px-7">
                             <p class="text-xl font-medium text-gray-800 md:text-2xl md:leading-normal xl:text-2xl xl:leading-normal dark:text-gray-200">
                                 To say that switching to Preline has been life-changing is an understatement. My business has tripled and I got my life back.
@@ -129,27 +107,22 @@ const Blogs = () => {
                                 Nicole Grazioso
                             </p>
                         </blockquote>
-
                         <figure>
                             <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1670272498380-eb330b61f3cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="Image Description" />
                             <figcaption class="mt-3 text-sm text-center text-gray-500">
                                 A man and a woman looking at a cell phone.
                             </figcaption>
                         </figure>
-
                         <div class="space-y-3">
                             <h3 class="text-2xl font-semibold dark:text-white">Bringing the culture of sharing to everyone</h3>
 
                             <p class="text-lg text-gray-800 dark:text-gray-200">We know the power of sharing is real, and we want to create an opportunity for everyone to try Preline and explore how transformative open communication can be. Now you can have a team of one or two designers and unlimited spectators (think PMs, management, marketing, etc.) share work and explore the design process earlier.</p>
                         </div>
-
                         <ul class="list-disc list-outside space-y-5 ps-5 text-lg text-gray-800 dark:text-gray-200">
                             <li class="ps-2">Preline allows us to collaborate in real time and is a really great way for leadership on the team to stay up-to-date with what everybody is working on," <a class="text-blue-600 decoration-2 hover:underline font-medium" href="#">said</a> Stewart Scott-Curran, Intercom's Director of Brand Design.</li>
                             <li class="ps-2">Preline opened a new way of sharing. It's a persistent way for everyone to see and absorb each other's work," said David Scott, Creative Director at <a class="text-blue-600 decoration-2 hover:underline font-medium" href="#">Eventbrite</a>.</li>
                         </ul>
-
                         <p class="text-lg text-gray-800 dark:text-gray-200">Small teams and individual designers need a space where they can watch the design process unfold, both for themselves and for the people they work with – no matter if it's a fellow designer, product manager, developer or client. Preline allows you to invite more people into the process, creating a central place for conversation around design. As those teams grow, transparency and collaboration becomes integrated in how they communicate and work together.</p>
-
                         <div>
                             <a class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
                                 Plan
@@ -165,14 +138,11 @@ const Blogs = () => {
                             </a>
                         </div>
                     </div>
-
                 </div>
             </div>
-
             <div class="sticky bottom-6 inset-x-0 text-center">
                 <div class="inline-block bg-white shadow-md rounded-full py-3 px-4 dark:bg-gray-800">
                     <div class="flex items-center gap-x-1.5">
-
                         <div class="hs-tooltip inline-block">
                             <button type="button" class="hs-tooltip-toggle flex items-center gap-x-2 text-sm text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
                                 <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" /></svg>
@@ -182,11 +152,7 @@ const Blogs = () => {
                                 </span>
                             </button>
                         </div>
-
-
                         <div class="block h-3 border-e border-gray-300 mx-3 dark:border-gray-600"></div>
-
-
                         <div class="hs-tooltip inline-block">
                             <button type="button" class="hs-tooltip-toggle flex items-center gap-x-2 text-sm text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
                                 <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z" /></svg>
@@ -196,11 +162,7 @@ const Blogs = () => {
                                 </span>
                             </button>
                         </div>
-
-
                         <div class="block h-3 border-e border-gray-300 mx-3 dark:border-gray-600"></div>
-
-
                         <div class="hs-dropdown relative inline-flex">
                             <button type="button" id="blog-article-share-dropdown" class="hs-dropdown-toggle flex items-center gap-x-2 text-sm text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
                                 <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" /><polyline points="16 6 12 2 8 6" /><line x1="12" x2="12" y1="2" y2="15" /></svg>
@@ -232,11 +194,9 @@ const Blogs = () => {
                                 </a>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };
